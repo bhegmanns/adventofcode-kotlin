3d9fa74c1c95d4ff9c93e59fa82c8223d2e491fb
Bernd Hegmanns <it@hegmanns.de>
2021-12-01 09:24:13 +0100

    solution for day 01 in 2021 not really the best solution try to create better solution at next freetime

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var preValue: Int = Int.MAX_VALUE
+        var currentValue: Int
+        var countIncreasedValues = 0
+
+        for (s in input) {
+            currentValue = s.toInt()
+            if (currentValue > preValue) {
+                countIncreasedValues++
+            }
+            preValue = currentValue
+        }
+        return countIncreasedValues
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val newList:MutableList<String> = mutableListOf()
+
+        for (currentIndex in IntRange(0, input.size - 3)) {
+            val subList = input.subList(currentIndex, currentIndex + 3)
+            val sum = subList.stream().mapToInt(String::toInt).sum()
+            newList.add("" + sum)
+        }
+
+        return part1(newList)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    pruefe(part1(testInput), 7)
+    pruefe(part2(testInput), 5)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,9 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun pruefe(givenValue: Int, expectedValue: Int) {
+    check(givenValue == expectedValue) {
+        "given: " + givenValue + " but expected: " + expectedValue
+    }
+}
